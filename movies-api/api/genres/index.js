a89c2f46dc1ae7845a8c99955c02d1ea6b2f5508
AbazBajrami <abaz.bajrami20@gmail.com>
2021-12-08 23:36:20 +0000

    Mongo lab Exercises

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { genres } from './genres';
+import Genre from './genreModel';
 
 
 const router = express.Router(); 
@@ -8,16 +9,27 @@ router.get('/', (req, res) => {
 });
 
 // Get movie genres
-router.get('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    if (genres.id == id) {
-        res.status(200).json(genres);
-    } else {
-        res.status(404).json({
-            message: 'The resource you requested could not be found.',
-            status_code: 404
-        });
-    }
+// router.get('/:id', (req, res) => {
+//     const id = parseInt(req.params.id);
+//     if (genres.id == id) {
+//         res.status(200).json(genres);
+//     } else {
+//         res.status(404).json({
+//             message: 'The resource you requested could not be found.',
+//             status_code: 404
+//         });
+//     }
+// });
+
+
+// Get all genres
+router.get('/', async (req, res) => {
+    const genres = await User.find();
+    res.status(200).json(users);
 });
+
+
+
+
 
 export default router;
